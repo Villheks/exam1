e7386bb597a2a4c25aa139664588cb4d238db21c
Ane Birgitte Haug Skoglund <aneskoglund@Anes-Air.uia.no>
2024-01-30 12:55:38 +0100

    finished

--- task4.cs
+++ task4.cs
@@ -1,8 +1,48 @@
-// Your program should start at this line.
+
+const int FullMoveSteps = 4;
+const int shortMove = 1;
+const int mediumMove = 3;
+const int longMove = 5;
+
+MoveAndTurn(FullMoveSteps, Turn);
+MoveAndTurn(mediumMove, TurnLeft);
+MoveAndTurn(FullMoveSteps, Turn);
+MoveAndTurn(shortMove, TurnLeft);
+MoveAndTurn(mediumMove, Turn);
+MoveAndTurn(shortMove, TurnLeft);
+MoveAndTurn(longMove, Turn);
+MoveAndTurn(shortMove, TurnLeft);
+MoveAndTurn(mediumMove, Turn);
+MoveAndTurn(shortMove, TurnLeft);
+MoveAndTurn(mediumMove, Turn);
+MoveAndTurn(shortMove, TurnLeft);
+MoveAndTurn(longMove, Turn);
+MoveAndTurn(shortMove, TurnLeft);
+MoveAndTurn(FullMoveSteps, Turn);
+MoveAndTurn(shortMove, TurnLeft);
+MoveAndTurn(mediumMove, Turn);
+MoveAndTurn(shortMove, TurnLeft);
+MoveAndTurn(longMove, Turn);
+MoveAndTurn(shortMove, TurnLeft);
+MoveAndTurn(FullMoveSteps, Turn);
+MoveAndTurn(shortMove, TurnLeft);
+MoveAndTurn(mediumMove, Turn);
+MoveAndTurn(shortMove, TurnLeft);
+MoveAndTurn(mediumMove, Turn);
+MoveAndTurn(shortMove, TurnLeft);
+MoveAndTurn(longMove, Turn);
+MoveAndTurn(shortMove, TurnLeft);
+
+// Check if at the goal
+if (AtGoal())
+{
+    Console.WriteLine("Winner!");
+    return;
+}
 
 
 #region Basic functions
-// These functions are just her to make your intelisense work. 
+// These functions are just here to make your IntelliSense work. 
 // They only have a conceptual function.
 
 void Move()
@@ -12,19 +52,43 @@ void Move()
 
 void Turn()
 {
+    
     // Turns the car 90 deg clockwise.
 }
 
 bool Peek()
 {
     // Returns true if the next cell is open, otherwise false.
-    return true; // Just a placeholder value. 
+    return false;
 }
 
 bool AtGoal()
 {
     // Returns true if the current cell is the goal cell.
-    return true; // just a placholder
+    return false; 
 }
 
+void TurnLeft()
+{
+    Turn();
+    Turn();
+    Turn();
+    
+}
+
+void MoveAndTurn(int moveSteps, Action turnAction)
+{
+    for (int i = 0; i < moveSteps; i++)
+    {
+        Move();
+    }
+    turnAction();
+}
+
+
 #endregion
+
+
+
+
+
